91e60fe002b0bf95f8d575eb08f30b7e94253630
Josh Matthias <jmatthias@lacoda.com>
2013-03-27 11:46:49 -0400

    'tspecs_from_callable' tests
    Add 'IOProcessFailureError', 'combine_tspecs' to package '__init__.py'
    imports.
    
    Add 'tspecs_from_callable' module.
    
    Add test to confirm that the result of 'tspecs_from_callable' is
    compatible with the parameters of 'IOProcessor.process'.
    
    Add tests for 'tspecs_from_callable' function. Confirm function result
    when 'callable_object' function has various representative combinations
    of parameters.

--- ioprocess/__init__.py
+++ ioprocess/__init__.py
@@ -1,4 +1,5 @@
 from ioprocess import (
+    IOProcessFailureError,
     CoercionFailureError,
     CoercionSuccessError,
     AnyType,
@@ -6,6 +7,7 @@ from ioprocess import (
     IOProcessor,
     input_processor,
     output_processor,
+    combine_tspecs,
     tspecs_from_callable,
     default_coercion_functions_input as default_input_coercion_functions,
     default_coercion_functions_output as default_output_coercion_functions,
---
+++ tests/test_tspecs_from_callable.py
@@ -0,0 +1,84 @@
+import pytest
+import unittest
+from ioprocess import (
+    IOProcessor,
+    AnyType,
+    tspecs_from_callable,
+    combine_tspecs,
+    )
+
+pytestmark = pytest.mark.a
+
+class ResultTest(object):
+    """ Confirm that the result dictionary is compatible with the
+        'IOProcessor.process' method parameters. """
+    def test_result(self):
+        result = tspecs_from_callable(self.callable)
+        
+        IOProcessor().process(**result)
+
+class TspecValueTest(unittest.TestCase):
+    def tspec_value_test(self, tspec_name, expected_list):
+        result = tspecs_from_callable(self.callable)
+        expected = {ikey: AnyType for ikey in expected_list}
+        assert result[tspec_name] == expected
+    
+
+class TestParametersEmpty(TspecValueTest, ResultTest):
+    @property
+    def callable(self):
+        def callable_obj():
+            pass
+        return callable_obj
+    
+    def test_required_tspec(self):
+        self.tspec_value_test('required', [])
+    
+    def test_optional_tspec(self):
+        self.tspec_value_test('optional', [])
+
+class TestParametersRequired(TspecValueTest, ResultTest):
+    @property
+    def callable(self):
+        def callable_obj(a):
+            pass
+        return callable_obj
+    
+    def test_required_tspec(self):
+        self.tspec_value_test('required', ['a'])
+    
+    def test_optional_tspec(self):
+        self.tspec_value_test('optional', [])
+
+class TestParametersOptional(TspecValueTest, ResultTest):
+    @property
+    def callable(self):
+        def callable_obj(b=None):
+            pass
+        return callable_obj
+    
+    def test_required_tspec(self):
+        self.tspec_value_test('required', [])
+    
+    def test_optional_tspec(self):
+        self.tspec_value_test('optional', ['b'])
+
+class TestParametersBothRequiredAndOptional(TspecValueTest, ResultTest):
+    @property
+    def callable(self):
+        def callable_obj(a, b=None):
+            pass
+        return callable_obj
+    
+    def test_required_tspec(self):
+        self.tspec_value_test('required', ['a'])
+    
+    def test_optional_tspec(self):
+        self.tspec_value_test('optional', ['b'])
+
+
+
+
+
+
+
