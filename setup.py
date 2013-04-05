d3a7fe689bf2a6b4b863e4b2ec8833c06682ef40
Josh Matthias <jmatthias@lacoda.com>
2013-04-05 01:08:13 -0400

    Prepare release v0.3.1
    Update version number.
    
    Update changelog.
    
    Update README.md.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iomanager',
-    version='0.3.0',
+    version='0.3.1',
     author='Josh Matthias',
     author_email='python.iomanager@gmail.com',
     packages=['iomanager'],
